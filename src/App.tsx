f06e0d2f9528c58bb32b7f57f378460d36004a68
MaxwellChrist <ribthulu@gmail.com>
2022-10-30 09:53:25 -0400

    imported necessary files and created file to store styled divs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+
+// Material UI Stuff
+import Drawer from '@mui/material/Drawer'
+import LinearProgress from '@mui/material/LinearProgress'
+import Grid from '@mui/material/Grid'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge from '@mui/material/Badge';
+
+// styled component stuff
+import Wrapper from './App.styles'
+
 function App() {
   return (
     <div className="App">
