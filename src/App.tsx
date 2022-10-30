91fd1ce98ed20dfdb641f3d1dbdd9b3a2e114205
MaxwellChrist <ribthulu@gmail.com>
2022-10-30 10:07:37 -0400

    successfully used useQuery to get products from fakestoreapi.com

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,24 @@ import Badge from '@mui/material/Badge';
 // styled component stuff
 import Wrapper from './App.styles'
 
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => 
+  await(await fetch('http://fakestoreapi.com/products')).json()
+
+
 function App() {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+  console.log(data)
+
   return (
     <div className="App">
       Beginning
